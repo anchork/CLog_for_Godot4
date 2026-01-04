c04368d6c037d7b89c51ea01c8eb0142ee1a5af6
anchork <73106040+anchork@users.noreply.github.com>
2026-01-05 08:48:31 +0900

    chore: modify pack_addon.sh to accept version argument

--- pack_addon.sh
+++ pack_addon.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+
+if [ -z "$1" ]; then
+	echo "Usage: $0 <version>"
+	exit 1
+fi
+
+VERSION=$1
+
 cp LICENSE ./addons/clog/
 cp README.md ./addons/clog/
+sed -i '' "s/version=\".*\"/version=\"$VERSION\"/" ./addons/clog/plugin.cfg
 zip -r clog.zip ./addons/clog -x "*.uid"
